00ec29c598db2af1182ff735d5692f0b04262706
Elias <eliazzlima@gmail.com>
2023-12-01 15:18:41 -0300

    Retorna a linha onde aconteceu o erro

--- pasrser_2.py
+++ pasrser_2.py
@@ -1,9 +1,10 @@
 from analisador_lexico import *
 
 class AnalisadorSintatico:
-    def __init__(self, tokens):
+    def __init__(self, tokens, codigo):
         self.tokens = tokens
         self.posicao = 0
+        self.codigo = codigo
 
     def analise_sintatica(self):
         try:
@@ -21,13 +22,15 @@ class AnalisadorSintatico:
         if self.tokens[self.posicao]['tipo'] == 'IDENTIFICADOR':
             self.avancar()
         else:
-            raise SyntaxError("Erro de sintaxe: Identificador inválido")
+            linha = self.carregar_linhas_codigo()
+            raise SyntaxError(f"Erro de sintaxe: Identificador inválido \"{self.tokens[self.posicao]['tipo']}\" na linha {linha}")
 
     def match(self, terminal):
         if self.tokens[self.posicao]['tipo'] == terminal:
             self.avancar()
         else:
-            raise SyntaxError(f"Erro de sintaxe: Esperado {terminal}, mas encontrado {self.tokens[self.posicao]['tipo']}")
+            linha = 1
+            raise SyntaxError(f"Erro de sintaxe: Esperado {terminal} na linha {linha}, mas encontrado {self.tokens[self.posicao]['tipo']}")
 
     def avancar(self):
         if self.posicao < len(self.tokens) - 1:
@@ -35,10 +38,31 @@ class AnalisadorSintatico:
         else:
             raise SyntaxError("Compilado")
 
+    def carregar_linhas_codigo(self):
+        linhas_codigo = []
+        # Ler o txt com o código
+        with open(self.codigo, 'r') as arquivo:
+            linha = arquivo.read()
+            # Remove os espaços do código
+            linhas_codigo = linha.replace(' ', '')
+
+            # Salva as linhas de código
+            linhas_codigo = linhas_codigo.split()
+
+            linha = 1
+            for i in linhas_codigo:
+                if i.find(self.tokens[self.posicao]['tipo']) >= 0:
+                    break
+                linha = linha + 1
+
+        return linha
+
+# Nome do arquivo contendo o código
+codigo = "codigo_2.txt"
 
 # Obtém os tokens do programa
 tokens_encontrados = carregar_tokens(programa_exemplo)
 
 # Crie uma instância do analisador sintático e realize a análise
-analisador = AnalisadorSintatico(tokens_encontrados)
+analisador = AnalisadorSintatico(tokens_encontrados, codigo)
 analisador.analise_sintatica()
